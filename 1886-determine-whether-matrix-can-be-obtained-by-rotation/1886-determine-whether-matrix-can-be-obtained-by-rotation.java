1ba5dee3be899252cc030fbe87a6949276175423
Vijayalaxmi Singh <177923622+Vijayalaxmi-ctrl@users.noreply.github.com>
2026-07-31 21:39:24 +0530

    Time: 1 ms (39.04%), Space: 43.5 MB (77.64%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.java
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -1,32 +1,31 @@
 class Solution {
     public boolean findRotation(int[][] mat, int[][] target) {
-        
-        for(int k = 0 ; k < 4; k++) {
-        boolean ans = true;
+        for(int k = 0; k < 4 ; k ++) {
+            boolean ans = true;
 
-        for(int i = 0; i < mat.length ; i++) {
-            for(int j = 0; j < mat.length ; j++) {
-                if(mat[i][j] != target[i][j]){
-                    ans = false;
+            for(int i = 0 ; i < mat.length; i++) {
+                for(int j = 0; j < mat.length ; j++) {
+                    if(mat[i][j] != target[i][j]) {
+                        ans = false;
+                        break;
+                    }
+                }
+                if (!ans) {
                     break;
                 }
             }
-        }
 
-        if (ans) {
-            return true;
-        }
-
-        int[][] arr = new int[mat.length][mat.length];
-        for(int i = 0 ; i < mat.length ; i++) {
-            for(int j = 0; j < mat.length; j++) {
-                arr[j][mat.length-1-i] = mat[i][j];
+            if(ans) {
+                return true;
             }
+            int[][] arr = new int[mat.length][mat.length];
+            for(int i = 0; i < mat.length; i++) {
+                for(int j = 0 ; j < mat.length ; j++) {
+                    arr[j][mat.length - 1 - i] = mat[i][j];
+                }
+            }
+            mat = arr;
         }
-        mat = arr;
-        }
-        
         return false;
     }
-
 }
