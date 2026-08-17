0a22c95584ae0420970918793ad31f044a03ecf3
Vijayalaxmi Singh <177923622+Vijayalaxmi-ctrl@users.noreply.github.com>
2026-08-17 15:33:53 +0530

    Time: 1 ms (99.89%), Space: 55.5 MB (60.12%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -12,15 +12,15 @@ class Solution {
                 cnt--;
             }
         }
-        int cnt1=0;
-        for(int i = 0 ; i < nums.length ; i++) {
-            if(nums[i] == el) {
-                cnt1++;
-            }
-        }
-        if(cnt1 > nums.length/2) {
-            return el;
-        }
-        return -1;
+        // int cnt1=0;
+        // for(int i = 0 ; i < nums.length ; i++) {
+        //     if(nums[i] == el) {
+        //         cnt1++;
+        //     }
+        // }
+        // if(cnt1 > nums.length/2) {
+        //     return el;
+        // }
+        return el;
     }
 }
