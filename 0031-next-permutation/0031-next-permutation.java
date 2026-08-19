dc50c8d93ea05f05f620b8080cb69c1f090b525c
Vijayalaxmi Singh <177923622+Vijayalaxmi-ctrl@users.noreply.github.com>
2026-08-19 08:09:32 +0530

    Time: 0 ms (100%), Space: 44.5 MB (85.61%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,8 +1,8 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int ind = -1;
-        for(int i = nums.length-2; i >= 0 ; i--) {
-            if(nums[i] < nums[i+1]){
+        for(int i = nums.length - 2; i >= 0;i--) {
+            if(nums[i] < nums[i+1]) {
                 ind = i;
                 break;
             }
@@ -11,8 +11,7 @@ class Solution {
             rev(nums,0,nums.length-1);
             return;
         }
-
-        for(int i = nums.length-1; i >= ind; i--) {
+        for(int i = nums.length - 1; i >= 0 ; i--) {
             if(nums[i] > nums[ind]) {
                 int temp = nums[i];
                 nums[i] = nums[ind];
@@ -23,13 +22,13 @@ class Solution {
         rev(nums,ind+1,nums.length-1);
     }
 
-    public void rev(int[] arr, int start , int end) {
-        while(start < end) {
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
+    public void rev(int[] arr, int a,int b) {
+        while(a < b) {
+            int temp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = temp;
+            a++;
+            b--;
         }
     }
 }
