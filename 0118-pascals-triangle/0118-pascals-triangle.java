c18d14a3b308d9ce931988f9bfc5422bab79eeeb
Vijayalaxmi Singh <177923622+Vijayalaxmi-ctrl@users.noreply.github.com>
2026-08-20 17:26:12 +0530

    Time: 1 ms (95.42%), Space: 43.7 MB (16.54%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int row = 1 ; row <= numRows ; row++) {
+            List<Integer> list = new ArrayList<>();
+            for(int col = 1; col <= row; col++) {
+                list.add(funNCR(row-1,col-1));
+            }
+            ans.add(list);
+        }
+        return ans;
+    }
+
+    public int funNCR(int n, int r) {
+        int res=1;
+        for(int i = 0; i < r ; i++) {
+            res=res*(n-i);
+            res = res/(i+1);
+        }
+        return res;
+    }
+}
