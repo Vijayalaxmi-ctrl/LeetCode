ef38d64aa15b12e53e23247805d3a48987d9b1a9
Vijayalaxmi Singh <177923622+Vijayalaxmi-ctrl@users.noreply.github.com>
2026-08-22 19:07:26 +0530

    Time: 9 ms (36.08%), Space: 49 MB (75.56%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -5,12 +5,12 @@ class Solution {
             (a,b) -> Integer.compare(a[0],b[0])
         );
         List<int[]> merged = new ArrayList<>();
-        for(int[] interval : intervals) {
-            if(merged.isEmpty() || merged.get(merged.size()-1)[1] < interval[0]) {
-                merged.add(new int[] {interval[0],interval[1]});
+        for(int i = 0 ; i  < intervals.length ; i++) {
+            if(merged.isEmpty() || merged.get(merged.size()-1)[1] < intervals[i][0]) {
+                merged.add(new int[] {intervals[i][0],intervals[i][1]});
             } else {
                 int last = merged.size()-1;
-                int max = Math.max(merged.get(last)[1],interval[1]);
+                int max = Math.max(merged.get(last)[1],intervals[i][1]);
                 merged.get(last)[1] = max;
         }
         }
