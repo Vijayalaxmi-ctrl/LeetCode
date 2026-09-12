34ae6a79aa4d186eb5fba551d4a6ada32aedc051
Vijayalaxmi Singh <177923622+Vijayalaxmi-ctrl@users.noreply.github.com>
2026-09-12 11:22:51 +0530

    Time: 1 ms (98.43%), Space: 43.3 MB (67.22%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -3,12 +3,8 @@ class Solution {
         if(s.length() != goal.length()) {
             return false;
         }
-       for(int i = 0 ;i < s.length(); i++) {
-        String rotated = s.substring(i) + s.substring(0,i);
-        if(rotated.equals(goal)){
-            return true;
-        }
-       }
-        return false;
+        String doubleS = s + s;
+        return doubleS.contains(goal);
     }
+   
 }
